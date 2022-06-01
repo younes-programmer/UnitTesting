a0ee01bbef2356fbf604982d7f6f73f1609b3a68
younes-programmer <younes.lagbouri@gmail.com>
2022-06-01 18:35:17 -0400

    another commit

--- app.py
+++ app.py
@@ -6,11 +6,6 @@ def equation_resolve(a, b):
     TODO
     Write a function that resolves equation of type ax + b = 0
     """
-    solutions = "non"
-    if a == 0:
-        return solutions
-    solutions = -b/a
-    return solutions
 
 
 def equation_resolve_second_degree(a, b, c):
@@ -18,10 +13,4 @@ def equation_resolve_second_degree(a, b, c):
     TODO
     Write a function that resolves equation of type ax^2 + bx + c = 0
     """
-    delta = b*b-4*a*c
-    if delta < 0:
-        return []
-    elif delta == 0:
-        return [-b/(2*a)]
-    else:
-        return[(-b-math.sqrt(delta))/(2*a), (-b+math.sqrt(delta))/(2*a)]
+
